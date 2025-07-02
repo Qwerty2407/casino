6d3f8ff9125dd65c6b5b029fbf3cbd3a80d5cd96
admin <nikitaklimkavich@gmail.com>
2025-07-02 10:31:51 +0200

    casino.py file update

--- casino.py
+++ casino.py
@@ -3,10 +3,13 @@ import tkinter
 
 
 class Game:
-    def __init__(self, jackpot_spins):
+    def __init__(self, jackpot_spins) -> None:
         self.num = None
         self.victory_list = None
         self.jackpot_spins = jackpot_spins
+        self.loss_list = ["banana.png", "cherry.png", "cigar.png", "grape.png", "limon.png", "raspberry.png",
+                          "strawberry.png", "watermelon.png"]
+        self.pictures = None
 
     def victory_check(self, result: int) -> None:
         while self.victory_list is None:
@@ -27,36 +30,33 @@ class Game:
     def victory(self, result: int) -> None:
         if result == self.victory_list[0]:
             self.num = random.randint(25, 40)
-            picture = "diamond.png"
+            self.pictures = ["diamond.png"] * 3
         elif result == self.victory_list[1]:
             self.num = random.randint(15, 25)
-            picture = "crown.png"
+            self.pictures = ["crown.png"] * 3
         elif result == self.victory_list[1]:
             self.num = random.randint(5, 15)
-            picture = "clover.png"
+            self.pictures = ["clover.png"] * 3
         else:
             self.num = random.randint(2, 5)
-            picture = "coins.png"
+            self.pictures = ["coins.png"] * 3
 
     def loss(self) -> None:
-        pass
+        while self.pictures is None:
+            self.pictures = [a, b, c] if len(
+                {a := random.choice(self.loss_list) + ["crown.png", "coins.png"],
+                 b := random.choice(self.loss_list + ["seven.png", "coins.png", "clover.png", "diamond.png"]),
+                 c := random.choice(self.loss_list)}) == 3 else None
 
 
 class GUI:
+    def __init__(self, mode: str, pictures: list[str]) -> None:
+        self.mode = mode
+        self.pictures = pictures
+
     def create(self):
-        window = tkinter.Tk()
-        window.geometry("600x700")
-        window.title('Paper, scissors, stone')
-
-        c = tkinter.Canvas(window, width=600, height=700, bg='green')
-        c.pack()
-
-        seven = tkinter.PhotoImage(file="images/seven.png")
-        c.create_image(190, 60, anchor=tkinter.NW, image=seven)
-
-        window.mainloop()
+        pass
 
 
 if __name__ == "__main__":
-    # GUI().create()
-    Game(random.randint(3, 6)).victory_check(random.randint(1, 13))
+    Game(random.randint(3, 6)).victory_check(random.randint(1, 25))
